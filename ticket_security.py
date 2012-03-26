2f772de257c35a2c0b7689f6cc7d78303da9ac64
Alastair Lynn <arplynn@gmail.com>
2012-03-26 18:35:59 +0100

    Added shared ticket security library

---
+++ ticket_security.py
@@ -0,0 +1,31 @@
+import hmac
+import hashlib
+
+def current_academic_year():
+    import time
+    t = time.gmtime()
+    return t.tm_year + (1 if t.tm_mon > 7 else 0)
+
+class TicketSigner(object):
+    def __init__(self, private_key = None, year = None):
+        self.year = year if year is not None else current_academic_year()
+        if private_key is None:
+            with open('ticket.key') as f:
+                private_key = f.read()
+        self.private_key = private_key
+        self.signer = hmac.new(self.private_key, '{0}:'.format(self.year), hashlib.sha256)
+
+    def _hmac(self, username):
+        signer = self.signer.copy()
+        signer.update(username)
+        return signer.hexdigest()
+
+    def sign(self, username):
+        return '{0}:{1}'.format(username, self._hmac(username))
+
+    def verify(self, digest):
+        username, hmac = digest.split(':')
+        if hmac != self._hmac(username):
+            raise ValueError("HMAC is not valid")
+        return username
+
