fcaa4553fcedf00fa968bb9048379432087b45ba
Chris Kirkham <chrisjameskirkham@gmail.com>
2012-03-26 18:54:42 +0100

    Added the beginings (arg parsing) of a ticket generation script

---
+++ generate.py
@@ -0,0 +1,27 @@
+from ticket_security import TicketSigner
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Generate a Srobo Ticket")
+
+    parser.add_argument('-o', '--output',
+                        help="Output SVG file")
+
+    parser.add_argument('-y', '--year',
+                        help="The competition (academic) year")
+
+    parser.add_argument('-k', '--private-key-file',
+                        help=("The private key file for use by the "
+                              "ticket signer"))
+
+    parser.add_argument('username', nargs='+',
+                        help="The username for the ticket")
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+if __name__ == '__main__':
+    main()
