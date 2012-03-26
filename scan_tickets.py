961fc35c03431037bf69108b27e674fccfd67078
Alastair Lynn <arplynn@gmail.com>
2012-03-26 21:14:59 +0100

    Added support for the QR code of doom

--- scan_tickets.py
+++ scan_tickets.py
@@ -1,4 +1,5 @@
 import ticket_security
+from hashlib import sha256
 
 def scan_tickets(binary = 'zbarcam', signer = None):
     import subprocess, sys
@@ -11,6 +12,11 @@ def scan_tickets(binary = 'zbarcam', signer = None):
         stdout = process.stdout.readline().strip()
         if stdout.startswith('QR-Code:'):
             data = stdout[8:]
+            # Ticket of doom
+            if sha256(data).hexdigest() == '6640cc0c536901fbc7c9f0fbe508a09c80be74656cb3ab915ec668503d54f91d':
+                import webbrowser
+                webbrowser.open('http://nyan.cat')
+                continue
             try:
                 yield signer.verify(data)
             except ValueError:
