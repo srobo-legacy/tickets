afc25cb3c51478c69a6d1eef7c3a9b41fd59c2d3
Chris Kirkham <chrisjameskirkham@gmail.com>
2012-03-26 22:07:36 +0100

    Added a Ticket class that manages SVG generation
    Keeping things simple, this class only generates SVGs.  Conversion
    to PDF can happen outside this class.

--- generate.py
+++ generate.py
@@ -1,5 +1,9 @@
 from ticket_security import TicketSigner
-import argparse
+from PyQRNative import QRCode, QRErrorCorrectLevel
+import argparse, StringIO, base64
+
+HMAC_SUBST_STR = "$$__HMAC__$$"
+QR_DATA_URI_STR = "$$__QR_DATA_URI_STR__$$"
 
 def get_args():
     parser = argparse.ArgumentParser(description="Generate a Srobo Ticket")
@@ -21,6 +25,64 @@ def get_args():
                         help="The username for the ticket")
 
     return parser.parse_args()
+
+
+
+class Ticket(object):
+    def __init__(self, username, year=None, private_key_file=None):
+        self.username = username
+        self.year = year
+        self.private_key_file = None
+
+
+    def hmac(self):
+        """Computes the HMAC for the ticket"""
+
+        private_key = None
+        if self.private_key_file is not None:
+            with open(private_key_file) as f:
+                private_key = f.read()
+        ts = TicketSigner(private_key=private_key, year=self.year)
+        return ts.sign(self.username)
+
+
+    def qr_data_uri(self):
+        """Generates a QR Code and returns it as a data URI"""
+
+        qr = QRCode(6, QRErrorCorrectLevel.L)
+        qr.addData(self.hmac())
+        qr.make()
+        im = qr.makeImage()
+
+        # the image in the data uri seems to be rotated, unintentionally.
+        im = im.rotate(180)
+
+        qr_string = StringIO.StringIO()
+        im.save(qr_string, format="PNG")
+        format = "data:image/png;base64,{0}"
+        return format.format(base64.b64encode(qr_string.getvalue()))
+
+
+    def generate_SVG(self, output_file, template='ticket_template.svg'):
+        """
+        Generates a SVG by performing substitutions on the given `template`,
+        writing it to `output`.
+        """
+
+        # read template SVG
+        with open(template, 'r') as f:
+            template_str = f.read()
+
+        # perform substitutions
+        template_str = template_str.replace(HMAC_SUBST_STR,
+                                            self.hmac())
+        template_str = template_str.replace(QR_DATA_URI_STR,
+                                            self.qr_data_uri())
+
+        # write output SVG
+        with open(output_file, 'w') as f:
+            f.write(template_str)
+
 
 
 def main():
