1fb86a6232237a7c5706e777cc02fb7d71d72862
Alastair Lynn <arplynn@gmail.com>
2012-03-27 15:14:44 +0100

    Converted the HMAC from hex to base64
    This is to shorten the output so we can use a smaller QR code.

--- ticket_security.py
+++ ticket_security.py
@@ -1,5 +1,4 @@
-import hmac
-import hashlib
+import hmac, hashlib, base64
 
 def current_academic_year():
     import time
@@ -13,12 +12,12 @@ class TicketSigner(object):
             with open('ticket.key') as f:
                 private_key = f.read()
         self.private_key = private_key
-        self.signer = hmac.new(self.private_key, '{0}:'.format(self.year), hashlib.sha256)
+        self.signer = hmac.new(self.private_key, '{0}\x1C'.format(self.year), hashlib.sha256)
 
     def _hmac(self, username):
         signer = self.signer.copy()
         signer.update(username)
-        return signer.hexdigest()
+        return base64.b64encode(signer.digest())
 
     def sign(self, username):
         return '{0}:{1}'.format(username, self._hmac(username))
