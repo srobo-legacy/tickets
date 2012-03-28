84c32af08edb2d603da0695c827b4bb92b4ab29f
Alastair Lynn <arplynn@gmail.com>
2012-03-28 16:00:25 +0100

    Moved to a simpler web API authentication scheme
    This removes the horrendous and unnecessary attempt at security that
    was there before.

--- webapi/horrendous.php
+++ webapi/horrendous.php
@@ -1,9 +1,21 @@
 <?php
 
+function deny($message) {
+    header("HTTP/1.1 418 $message");
+    header('Content-type: text/plain');
+    header('Content-length: ' . (strlen($message)) + 1);
+    echo "$message\n";
+    exit();
+}
+
 $config = parse_ini_file('config.ini');
 
 $data = $HTTP_RAW_POST_DATA;
+if (!$data) {
+    deny('No data.');
+}
 $key = file_get_contents($config['key']);
+$userdir = $config['users_dir'];
 
 $ldap_connection = ldap_connect($config['ldap_host'],
                                 isset($config['ldap_port']) ?
@@ -11,33 +23,39 @@ $ldap_connection = ldap_connect($config['ldap_host'],
                                     389);
 ldap_bind($ldap_connection, $config['ldap_rdn'], $config['ldap_pw']);
 
-$iv = substr($data, 0, 16);
-$data = substr($data, 16);
+$matched = substr($data, 0, strlen($key)) == $key;
+if (!$matched) {
+    deny('Nope.');
+}
+$data = substr($data, strlen($key));
 
-$decrypted = mcrypt_decrypt(MCRYPT_RIJDAEL_256, $key, $data,
-                            MCRYPT_MODE_CBC, $iv);
-
-$arguments = json_decode($decrypted);
+$arguments = json_decode($data);
 
 $username = $arguments['username'];
 $query = str_replace('?', $username, $config['ldap_query']);
 
 if (!empty($arguments['scanned'])) {
+    date_default_timezone_set('UTC');
+    file_put_contents("$user_dir/$username", date('r'));
     $output = 'cannot scan in yet';
 } else {
     $results_id = ldap_search($ldap_connection, $query, 'uid=*');
     $results = ldap_get_entries($ldap_connection, $results_id);
-    $output = array('username'  => $results[0]['uid'][0],
-                    'fullname'  => $results[0]['cn'][0] . ' ' .
-                                   $results[0]['sn'][0],
-                    'media_consent' => true,
-                    'checked_in' => false);
+    $output = array();
+    if (count($results) > 0) {
+        $username = $results[0]['uid'][0];
+        $outputs['username'] = $username;
+        $outputs['fullname'] = $results[0]['cn'][0] . ' ' . $results[0]['sn'][0];
+    } else {
+        $outputs['fullname'] = $username;
+    }
+    $outputs['checked_in'] = file_exists("$user_dir/$username");
 }
 
-$output = json_encode($output);
+$output = json_encode($output, JSON_FORCE_OBJECT);
 
 header('Content-type: application/json');
 header('Content-length: ' . strlen($output));
 
-echo json_encode($output);
+echo $output;
 
