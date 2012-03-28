5321ff28b14a2008173e7fc94faa7628db4fc3b5
Chris Kirkham <chrisjameskirkham@gmail.com>
2012-03-28 01:51:10 +0100

    Added a script to generate tickets in a 3up arrangement
    Given a list of usernames, this script will generate a PDF of
    tickets for all of the users listed and save some trees.
    Teachers could be sent tickets for their students in this way.

---
+++ generate3up.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+from generate import Ticket
+from ticket_security import current_academic_year
+import argparse
+import sys, os, subprocess, shutil
+
+def merge_ps_files(output_fname, input_fnames):
+    """
+    Given a series of input ps files, merge them into a single
+    ps file, 3up, on A4 paper, and save it as `output_fname`.
+    """
+
+    if len(input_fnames) == 0:
+        raise ValueError("Input files needed")
+
+    cmd_format = ("psmerge {files} | "
+                  "psnup -r"
+                  " -W{ticket_width}"
+                  " -H{ticket_height}"
+                  " -m{margin}"
+                  " -p{paper_size}"
+                  " -{n} -s1 "
+                  "> {output}")
+
+    call_dict = {'files':        "'" + "' '".join(input_fnames) + "'",
+                 'ticket_width':  '15cm',
+                 'ticket_height': '7.5cm',
+                 'margin':        '3cm',
+                 'paper_size':    'a4',
+                 'n':             '3',
+                 'output':        output_fname}
+
+    subprocess.check_call(cmd_format.format(**call_dict), shell=True)
+
+
+def ps2pdf(input_fname, output_fname):
+    """Call ps2pdf on a ps file and save it somewhere."""
+
+    cmd_format = "ps2pdf -dPDFX '{input}' '{output}'"
+    subprocess.check_call(cmd_format.format(input=input_fname,
+                                            output=output_fname), shell=True)
+
+
+def generate_and_merge_ps(output_fname, usernames, year, comp_date_str,
+                          private_key_file=None):
+    """
+    Given a list of usernames, and the standard ticket generation arguments,
+    create a postscript ticket for each username and merge them all into a
+    3up arangement.  The output is a postscript file.
+    """
+
+    files = []
+    tmp_dir = os.tmpnam()
+    os.mkdir(tmp_dir)
+
+    for username in usernames:
+        t = Ticket(username, year, comp_date_str,
+                   private_key_file=private_key_file)
+
+        path = os.path.join(tmp_dir, "{0}.ps".format(username))
+        files.append(path)
+        t.generate_PS(path)
+
+    merge_ps_files(output_fname, files)
+    shutil.rmtree(tmp_dir)
+
+
+
+def main():
+
+    # args
+    parser = argparse.ArgumentParser(description="Generate 3up ticket PDFs")
+
+    parser.add_argument('-y', '--year', default=current_academic_year(),
+                        help="The academic year of the competition")
+    parser.add_argument('-d', '--comp-date-str', default="",
+                        help="A string representing the date of the competition")
+    parser.add_argument('-k', '--private-key-file',
+                        help=("The private key file for use by the "
+                              "ticket signer"))
+    parser.add_argument('output',
+                        help="The output PDF file")
+    parser.add_argument('usernames', nargs='+',
+                        help="The usernames to add to the 3up sheet")
+
+    args = parser.parse_args()
+
+    # create 3up ticket sheets
+    merged_ps_file = os.tmpnam()
+    generate_and_merge_ps(merged_ps_file, args.usernames, args.year,
+                          args.comp_date_str,
+                          private_key_file=args.private_key_file)
+
+    ps2pdf(merged_ps_file, args.output)
+    os.remove(merged_ps_file)
+
+
+if __name__ == '__main__':
+    main()
