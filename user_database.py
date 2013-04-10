af8e387fdac336b2c8c35837b16afbaeadc6c8a7
orchard <chris.s.orchard@gmail.com>
2013-04-10 21:46:10 +0100

    Updated user_database script with real API URL
    Now points to studentrobotics.org rather than localhost

--- user_database.py
+++ user_database.py
@@ -2,7 +2,7 @@ import collections, json
 
 with open('ticket.key', 'r') as f:
     _key = f.read()
-_BASE_URI = "https://localhost/ticket-api/horrendous.php"
+_BASE_URI = "https://studentrobotics.org/ticket-api/horrendous.php"
 
 class User:
     _shared_users = {}
