5d64ff2a18a5967500109cc7e4ef3a62b82cf86b
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-02-03 03:26:18 -0800

    Eliminate the '_shared_users' dict.
    This looks to have been intended as being a caching layer. Given that
    tickets runs as a command line utility (right now) this is ineffective,
    and even if it were I would argue that it was wrong to do caching at
    this layer.

--- user_database.py
+++ user_database.py
@@ -5,7 +5,6 @@ with open('ticket.key', 'r') as f:
 _BASE_URI = "https://localhost/ticket-api/horrendous.php"
 
 class User:
-    _shared_users = {}
 
     @classmethod
     def _encode_query(self, query):
@@ -13,25 +12,23 @@ class User:
 
     @classmethod
     def get(self, username):
-        if username not in self._shared_users:
-            user = self(username)
-            import urllib2
-            request = urllib2.Request(_BASE_URI,
-                                      self._encode_query({'username': username}),
-                                      {'Content-type': 'application/octet-stream'})
-            f = urllib2.urlopen(request)
-            raw_data = f.read()
-            print raw_data
-            data = json.loads(raw_data)
-            f.close()
-            del raw_data, f
-            user.username = data['username'] if 'username' in data else username
-            user.fullname = data['fullname'] if 'fullname' in data else user.username
-            user.organisation = data['organisation'] if 'organisation' in data else 'Guest'
-            user.checked_in = data['checked_in'] if 'checked_in' in data else False
-            user.media_consent = data['media_consent'] if 'media_consent' in data else False
-            self._shared_users[username] = user
-        return self._shared_users[username]
+        user = self(username)
+        import urllib2
+        request = urllib2.Request(_BASE_URI,
+                                  self._encode_query({'username': username}),
+                                  {'Content-type': 'application/octet-stream'})
+        f = urllib2.urlopen(request)
+        raw_data = f.read()
+        print raw_data
+        data = json.loads(raw_data)
+        f.close()
+        del raw_data, f
+        user.username = data['username'] if 'username' in data else username
+        user.fullname = data['fullname'] if 'fullname' in data else user.username
+        user.organisation = data['organisation'] if 'organisation' in data else 'Guest'
+        user.checked_in = data['checked_in'] if 'checked_in' in data else False
+        user.media_consent = data['media_consent'] if 'media_consent' in data else False
+        return user
 
     def __init__(self, username):
         self.username = username
