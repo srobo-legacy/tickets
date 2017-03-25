9c1ecd969a2e267d1703ad36f489dbe8de7ba3dd
Richard Barlow <richard@richardbarlow.co.uk>
2017-03-25 12:46:15 +0000

    Remove caching of user data
    This caching layer allows the same ticket to be scanned on separate
    machines. It is not necessary.

--- user_database.py
+++ user_database.py
@@ -5,33 +5,29 @@ with open('ticket.key', 'r') as f:
 _BASE_URI = "https://studentrobotics.org/ticket-api/horrendous.php"
 
 class User:
-    _shared_users = {}
-
     @classmethod
     def _encode_query(self, query):
         return _key + json.dumps(query)
 
     @classmethod
     def get(self, username):
-        if username not in self._shared_users:
-            user = self(username)
-            import urllib2
-            request = urllib2.Request(_BASE_URI,
-                                      self._encode_query({'username': username}),
-                                      {'Content-type': 'application/octet-stream'})
-            f = urllib2.urlopen(request)
-            raw_data = f.read()
-            print raw_data
-            data = json.loads(raw_data)
-            f.close()
-            del raw_data, f
-            user.username = data['username'] if 'username' in data else username
-            user.fullname = data['fullname'] if 'fullname' in data else user.username
-            user.organisation = data['organisation'] if 'organisation' in data else 'Guest'
-            user.checked_in = data['checked_in'] if 'checked_in' in data else False
-            user.media_consent = data['media_consent'] if 'media_consent' in data else False
-            self._shared_users[username] = user
-        return self._shared_users[username]
+        user = self(username)
+        import urllib2
+        request = urllib2.Request(_BASE_URI,
+                                  self._encode_query({'username': username}),
+                                  {'Content-type': 'application/octet-stream'})
+        f = urllib2.urlopen(request)
+        raw_data = f.read()
+        print raw_data
+        data = json.loads(raw_data)
+        f.close()
+        del raw_data, f
+        user.username = data['username'] if 'username' in data else username
+        user.fullname = data['fullname'] if 'fullname' in data else user.username
+        user.organisation = data['organisation'] if 'organisation' in data else 'Guest'
+        user.checked_in = data['checked_in'] if 'checked_in' in data else False
+        user.media_consent = data['media_consent'] if 'media_consent' in data else False
+        return user
 
     def __init__(self, username):
         self.username = username
